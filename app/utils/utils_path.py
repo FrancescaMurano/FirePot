9781d3a2ac40f70decd88307818ba59e1f52f880
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-11-14 10:54:05 +0100

    add fake path directory on display

--- app/utils/utils_path.py
+++ app/utils/utils_path.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
 
 class Path:
 
     _instance = None
+    DISPLAY_PATH =  '\r\ndebian@root: '
 
     def __new__(cls) -> None:
         if not  cls._instance:
@@ -14,6 +16,7 @@ class Path:
             self.__start_full_path = os.path.join(os.getcwd(),"app","home")
             self.__current_path = self.__start_full_path
             self.initialized = True
+            self.__path_display = self.DISPLAY_PATH
 
     def get_start_full_path(self)-> str:
         return self.__start_full_path
@@ -23,8 +26,14 @@ class Path:
     
     def set_current_path(self,path):
         self.__current_path = os.path.join(self.__start_full_path,path)
+        self.__path_display = os.path.join(self.__path_display.replace(" ",""), os.path.basename(path))+"$ "
         
     def reset_path(self):
         self.__current_path = self.__start_full_path
+        self.__path_display = self.DISPLAY_PATH
+
+    def get_cli_display_path(self):
+        print("path display ", self.__path_display)
+        return self.__path_display
 
 
