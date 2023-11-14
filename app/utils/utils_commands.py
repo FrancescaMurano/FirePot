ecbec77ea3c2c6e20e550cc736fd32e585a4b16f
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-11-14 11:28:47 +0100

    add -C to ls command

--- app/utils/utils_commands.py
+++ app/utils/utils_commands.py
@@ -45,16 +45,19 @@ def exec_command(cmd: str):
             output = result.stdout.decode("utf-8")
             error = result.stderr.decode("utf-8")
 
-        elif cmd.startswith("ls") or cmd.startswith("dir") :
+        elif cmd.startswith("ls"):
 
             # to force the space elimination
-            if cmd.startswith("ls"):
-                cmd.join(" -C")
+            if cmd.find("-R"):
+                cmd = "ls -C -R"
+            else:
+                cmd = "ls -C"
                 
             result = subprocess.run(cmd, shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
             result.check_returncode()
             output = result.stdout.decode("utf-8")
             error = result.stderr.decode("utf-8")
+            
 
         else:
             result = subprocess.run(cmd, shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
