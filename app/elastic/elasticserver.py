73fcb2caf6ea7f0b234e8b42b38ce3f490af4f8e
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-11-24 17:19:46 +0100

    fix

--- app/elastic/elasticserver.py
+++ app/elastic/elasticserver.py
@@ -1,7 +1,7 @@
 from elasticsearch import Elasticsearch
 
+IP = "34.17.54.125"
 class ElasticServer:
-
     _instance = None
 
     def __new__(cls) -> None:
@@ -13,11 +13,11 @@ class ElasticServer:
         if not hasattr(self,'initialized'):
             self.initialized = True
 
-            username = ""
-            password = ""
+            username = "elastic"
+            password = "+RIk0Zh-KI=oJL*bkopF"
 
-            self.es = Elasticsearch("https://34.17.58.83:9200",
-                    ca_certs="config/certs/http_ca.crt",
+            self.es = Elasticsearch(f"http://{IP}:9200",
+                    # ca_certs="config/certs/http_ca.crt",
                     basic_auth=(username,password))
 
     def insert_ip_data(self,json_ip_data):
@@ -46,8 +46,6 @@ class ElasticServer:
         
     # def delete(self,id):
     #     self.es.delete(index='commands',id=id)
-
-
 
 
 
