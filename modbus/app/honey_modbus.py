6148b118235feee60b930eacee5a9054cb60c785
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-11-27 15:20:55 +0100

    add thread to server

--- modbus/app/honey_modbus.py
+++ modbus/app/honey_modbus.py
@@ -1,11 +1,13 @@
 
 
+from threading import Thread
 from modbus.server_slave import ServerSlave
 
 
 def main():
     s = ServerSlave()
-    s.run_server()
+    t1 = Thread(target=s.run_server)
+    t1.start()
    
 
 if __name__ == "__main__":
