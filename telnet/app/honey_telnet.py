db541a5c6939b58a309e61ee63599d8b30778475
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-11-30 17:07:26 +0100

    fix momentaneo da ricontrollare - problem socket send

--- telnet/app/honey_telnet.py
+++ telnet/app/honey_telnet.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+import socket
 from subprocess import *
 from elastic.elasticserver import ElasticServer
 from utils.utils_path import *
@@ -17,81 +18,95 @@ RIGHT_KEY = "\x1b[C".encode()
 LEFT_KEY = "\x1b[D".encode()
 BACK_KEY = "\x7f".encode()
 
- 
-
 async def handle_client(reader, writer):
-    
     ip = writer.get_extra_info('peername')[0]
     elastic = ElasticServer()
-    request = Request(ip)   
+    request = Request(ip)
 
     output = ""
 
     START = p.get_cli_display_path().encode('utf-8')
     writer.write(START)
-     
-    while True:
-        try:
-            command : bytes = await reader.read(100000)
+
+    try:
+        while True:
+            command: bytes = await reader.read(100000)
             if not command:
                 print("no command")
                 break
 
-            # excape commands
+            # escape commands
             if command == UP_KEY or command == DOWN_KEY or command == LEFT_KEY or command == RIGHT_KEY:
                 continue
 
             if command == b"\r\n":
                 multiple_cmds = re.split(r"&&", output)
                 results = []
-                print("multiple",multiple_cmds)
+                print("multiple", multiple_cmds)
                 multiple_cmds = [item for item in multiple_cmds if item != '']
                 for cmd in multiple_cmds:
                     cmd = cmd.strip()
-                    result,error = exec_command(cmd)
-                    elastic.insert_ip_request(request.get_request_json(cmd))
-                    results.append(result)
-                
+                    try:
+                        result, error = exec_command(cmd)
+                        elastic.insert_ip_request(request.get_request_json(cmd))
+                        results.append(result)
+                    except CalledProcessError as e:
+                        writer.write("Error: the sintax of the command is incorrect\r\n".encode("utf-8"))
+                        writer.write(p.get_cli_display_path().encode('utf-8'))
+                        print(f"Error: {str(e)}")
+                        output = ""
+
                 for res in results:
                     print("ok")
                     res = res.encode("utf-8")
-                    res = res.replace(b"  ",b"")
-                    res = res.replace(b"\n",b"\r\n")
+                    res = res.replace(b"  ", b"")
+                    res = res.replace(b"\n", b"\r\n")
                     writer.write(res)
-                
-                writer.write(p.get_cli_display_path().encode('utf-8'))
 
+                writer.write(p.get_cli_display_path().encode('utf-8'))
                 output = ""
 
-            elif command == b"\x08": # cancel only user input 
-                if output: 
+            elif command == b"\x08":  # cancel only user input
+                if output:
                     output = output[:-1]
-                    print("out",output)
+                    print("out", output)
                     writer.write(b' \x08')
                     writer.write(b' \x08')
 
-            elif command == b'\x03': # command to quit
-                    writer.write("\r\n".encode('utf-8'))
-                    writer.close()
-                     
-            else: # concat imput
-                output+= command.decode('utf-8')
+            elif command == b'\x03':  # command to quit
+                writer.write("\r\n".encode('utf-8'))
+                break  # exit the loop when client sends Ctrl+C
 
-        except Exception as e:
-            writer.write("Error: the sintax of the command is incorrect\r\n".encode("utf-8"))
-            writer.write(p.get_cli_display_path().encode('utf-8'))
-            print(f"Error: {str(e)}")
-            output = ""
-        
-    elastic.insert_ip_data(request.get_ip_info())
+            else:  # concat input
+                output += command.decode('utf-8')
 
-    print("Closing connection")
-    writer.close()
- 
- 
+    except asyncio.CancelledError:
+        pass  # Handle cancellation errors gracefully
+
+    except (ConnectionResetError, BrokenPipeError):
+        pass  # Handle specific socket errors
+
+    # except Exception as e:
+    #     writer.write(f"Error: {str(e)}\r\n".encode("utf-8"))
+    #     writer.write(p.get_cli_display_path().encode('utf-8'))
+    #     print(f"Error: {str(e)}")
+    #     output = ""
+
+    finally:
+        elastic.insert_ip_data(request.get_ip_info())
+        print("Closing connection")
+        writer.close()
+
 async def start_server():
     server = await asyncio.start_server(handle_client, "0.0.0.0", PORT)
     print("Server started")
     await server.serve_forever()
- 
-asyncio.run(start_server())
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(start_server())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
