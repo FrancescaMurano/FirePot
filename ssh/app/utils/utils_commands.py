81c125476902d5cc210b48ff155cd8ca8f8a18cd
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-04 08:51:27 +0100

    add commands and fix

--- ssh/app/utils/utils_commands.py
+++ ssh/app/utils/utils_commands.py
@@ -1,18 +1,12 @@
 import subprocess
 import os
+from .commands import WHITELIST_COMMANDS, IFCONFIG_FULL_RESPONSE,IP_A_RESPONSE,IFCONFIG_SIMPLE_RESPONSE
+
 import re
 from utils.utils_path import Path
-ERROR = "Error: the sintax of the command is incorrect"
 
-WHITELIST_COMMANDS = {
-    "cd": ["cd keys","cd payments","cd users"],
-    "ls": ["ls"],
-    "cat": ["cat psw.txt", "cat credit_cards.json", "cat p_key.pkcs1", "cat user.txt"],
-    "echo": ["echo"],
-    "clear": ["clear"],
-    "pwd": ["pwd"],
-    "whoami": ["whoami"],
-}
+ERROR_GEN = "Error: The sintax of the command is incorrect."
+ERROR_PATH = "Error: The system cannot find the path specified."
 
 def check_command(cmd: str):
     echo_pattern = r'^echo\s+[A-Za-z0-9\s]+$'
@@ -33,47 +27,58 @@ def exec_command(cmd: str):
     path = Path()
     output = ""
     error = ""
-    if check_command(cmd.strip()):
+    cmd = cmd.strip()
+    if check_command(cmd):
 
         if cmd.startswith("cd"):
             destination =  cmd.split(" ")[1] if len(cmd.split(" ")) > 1  else ''
-            path.set_current_path(os.path.join(path.get_start_full_path(),destination))
-            result = subprocess.run(cmd, shell=True,cwd=path.get_start_full_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-            result.check_returncode()
+            result = subprocess.run(cmd, shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+                
             output = result.stdout.decode("utf-8")
             error = result.stderr.decode("utf-8")
 
-        elif cmd.startswith("ls"):
+            if error == "":
+                destination = re.search(r"[a-zA-Z]+",destination).group()
+                path.set_current_path(os.path.join(path.get_current_path(),destination))
+            
+        elif cmd == "ifconfig":
+            output = IFCONFIG_SIMPLE_RESPONSE
 
-            result = subprocess.run("ls", shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-            result.check_returncode()
+        elif cmd == "ifconfig -a":
+            output = IFCONFIG_FULL_RESPONSE
+        
+        elif cmd == "ip a":
+            output = IP_A_RESPONSE
+            
+        elif cmd.startswith("ls"):
+            result = subprocess.run(cmd, shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+            # result.check_returncode()
             output = result.stdout.decode("utf-8")
             error = result.stderr.decode("utf-8")
         
         elif cmd.startswith("pwd"):
             output = "home"
-            
+
+        elif cmd.startswith("id"):
+            output = "uid=0(root) gid=0(root) gruppi=0(root)"
+              
         else:
             result = subprocess.run(cmd, shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-            result.check_returncode()
+            # result.check_returncode()
             output = result.stdout.decode("utf-8")
             error = result.stderr.decode("utf-8")
 
     else:
-        if cmd.startswith("cd"):
+        if cmd.startswith("cd") and cmd.find("..")!=-1:
             path.reset_path()
             result = subprocess.run("cd .", shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-            result.check_returncode()
+            # result.check_returncode()
             output = result.stdout.decode("utf-8")
             error = result.stderr.decode("utf-8")
 
-        elif cmd.startswith("ls"):
-            result = subprocess.run("ls", shell=True,cwd=path.get_current_path(), stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
-            result.check_returncode()
-            output = result.stdout.decode("utf-8")
-            error = result.stderr.decode("utf-8")
+        elif cmd.startswith("cd") and cmd.find("..")==-1:
+            output = ERROR_PATH
 
-        else:
-            output = ERROR
-
+        elif error == "":
+                output = ERROR_GEN
     return (output,error)
