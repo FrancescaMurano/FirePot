c29acdce896824bb1b7d89c880aa9751b56f5b13
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-06 15:27:18 +0100

    .

--- ftp/app/prova.py
+++ ftp/app/prova.py
@@ -1,34 +1,28 @@
-from twisted.protocols.ftp import FTP, FTPFactory, FTPRealm, DTP
+from asyncssh import SFTPError
+from twisted.protocols.ftp import FTP, FTPFactory, FTPRealm
 from twisted.cred.portal import Portal
 from twisted.cred.checkers import AllowAnonymousAccess
 from twisted.internet import reactor
 
-class MyDTP(DTP):
-    def __init__(self, consumer, offset=0):
-        super(MyDTP, self).__init__(consumer, offset)
-        self.upload_allowed = True
-
-    def fileReceived(self, data):
-        """
-        Sovrascrivi questo metodo per personalizzare il comportamento dell'upload di file.
-        """
-        if self.upload_allowed:
-            # Applica qui la logica desiderata per l'upload del file
-            print("File received. Applying custom logic.")
-            # In questo esempio, il file viene effettivamente ricevuto
-            super(MyDTP, self).fileReceived(data)
-        else:
-            print("File upload not allowed.")
-
 class MyFTP(FTP):
     def ftp_STOR(self, path):
         """
-        Sovrascrivi questo metodo per gestire l'inizio del comando STOR.
+        Sovrascrivi questo metodo per gestire il comando STOR (PUT).
         """
-        self.dtpInstance = MyDTP(self, self.dtpInstance.consumer.offset)
-        self.sendLine("150 Ok to send data")
+        # Esegui la tua logica personalizzata qui
+        try:
+            self.dtpInstance = self.dtpFactory.buildDTPConnection(self)
+            self.dtpInstance.startReceiver()
+            self.dtpInstance.consumer = self.file_system.openForWritingAscii(path)
+        except FileNotFoundError:
+            self.respond("550 File not found")
+        except PermissionError:
+            self.respond("550 Permission denied")
+        except SFTPError as e:
+            self.respond(f"550 {e}")
+        else:
+            self.respond("150 File status okay; about to open data connection.")
 
-# Definisci un realm personalizzato per FTP
 class MyFTPRealm(FTPRealm):
     def buildProtocol(self, addr):
         p = MyFTP()
