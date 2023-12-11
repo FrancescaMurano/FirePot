83f0e6902ed66aa36d3967ea06f50664b85845cb
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-11 12:50:41 +0100

    fix errors in async version

--- ssh/app/honeycopia.py
+++ ssh/app/honeycopia.py
@@ -22,6 +22,9 @@ BACK_KEY = "\x7f".encode()
 class SSHServer(asyncssh.SSHServer):
     def __init__(self):
         super().__init__()
+    def connection_made(self, conn):
+        conn.send_auth_banner('SSH-2.0-OpenSSH_5.3\n')
+        super().connection_made(conn)
 
     def session_requested(self):
         return SSHSession()
@@ -30,7 +33,6 @@ class SSHServer(asyncssh.SSHServer):
         return True
 
     def validate_password(self, username, password):
-        # Valida la password (in questo esempio, accetta qualsiasi password per l'utente "root")
         return username == 'root' and password == 'root'
 
    
@@ -44,8 +46,8 @@ class SSHSession(asyncssh.SSHServerSession):
 
     def connection_made(self, chan):
             self.chan = chan
-            # Get the client's address (IP and port)
             peer_addr = chan.get_extra_info('peername')
+            self.chan.write(self.path.get_cli_display_path())
             if peer_addr:
                 self.client_ip = peer_addr[0]
             else:
@@ -59,36 +61,33 @@ class SSHSession(asyncssh.SSHServerSession):
     def shell_requested(self) -> bool:
         return True
 
-
-    def get_banner(self):
-        return ("SSH-2.0-OpenSSH_5.3\n",'en-US')
-
     def data_received(self, data:str, datatype):
         try:
             command = data.encode("utf-8")
-            if command.endswith(b"\n"):
+
+            if command == b'\x03\n' or command == b'exit\n':  # command to quit
+                self.chan.exit(0)
+                
+            elif command.endswith(b"\n"):
                 self.chan.write("\r\n")
                 multiple_cmds = re.split(r"&&", data)
                 results = []
 
                 for cmd in multiple_cmds:
                     cmd = cmd.lstrip()
-                    print("cmd ",cmd)
                     result, error = exec_command(cmd)
-                    print("result ",result)
                     if error:
                         print("error", error)
-                    #self.server.insert_ip_request(self.request.get_request_json(cmd))
+                    self.server.insert_ip_request(self.request.get_request_json(cmd))
                     results.append(result)
 
                 for res in results:
                     res = res.encode("utf-8")
-                    res = res.replace(b"  ", b"")
-                    res = res.replace(b"\n", b"\r\n")
+                    # res = res.replace(b"  ", b"")
+                    # res = res.replace(b"\n", b"\r\n")
                     self.chan.write(res.decode("utf-8"))
 
                 self.chan.write(self.path.get_cli_display_path())
-                # self.output = ""
 
             elif command == b"\x7f":  # cancel only user input
                 if self.output:
@@ -96,25 +95,15 @@ class SSHSession(asyncssh.SSHServerSession):
                     self.chan.write(b'\x08')
                     self.chan.write(b' \x08')
 
-            elif command == b'\x03' or command == b"exit":  # command to quit
-                self.chan.write("\r\n".encode('utf-8'))
-                self.chan.exit(0)
             
         except Exception as e:
             print(str(e))
-
-        # else:  # concat input
-        #     self.output += command
-        #     self.chan.write(data)
-
 
 async def start_server():
     await asyncssh.create_server(
         lambda *args, **kwargs: SSHServer(),
         '0.0.0.0', PORT,
-        server_host_keys=['static_host_key'],
-        # authorized_client_keys='ssh_user_ca'
-    )
+        server_host_keys=['static_host_key'])
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
