aaef40328b62b4357762d52fe7200f35eb6959f0
FrancescaMurano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-12 06:33:13 -0600

    script

--- load_dash.sh
+++ load_dash.sh
@@ -4,12 +4,17 @@ KIBANA_HOST="localhost"
 KIBANA_PORT="5601"
 
 # Indica il file JSON della dashboard
-DASHBOARD_FILE="/dashboards/ftp_dashboard.ndjson"
+DASHBOARD_FTP_FILE="dashboards/ftp_dashboard.ndjson"
+DASHBOARD_SSH_FILE="dashboards/modbus_dashboard.ndjson"
+DASHBOARD_TELNET_FILE="dashboards/ssh_dashboard.ndjson"
+DASHBOARD_MODBUS_FILE="dashboards/telnet_dashboard.ndjson"
 
 # Componi l'URL per l'API di Kibana
-# KIBANA_API_URL="http://${KIBANA_HOST}:${KIBANA_PORT}/api/saved_objects/_import"
+KIBANA_API_URL="http://${KIBANA_HOST}:${KIBANA_PORT}/api/saved_objects/_import"
 
 # Invia la richiesta di importazione usando curl
-# curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_FILE}
+curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_FTP_FILE}
+curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_SSH_FILE}
+curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_MODBUS_FILE}
+curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_TELNET_FILE}
 
-cat "${DASHBOARD_FILE}"
