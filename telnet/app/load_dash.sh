056ea5f2e8431de04d085df0e1cba2363c9b252e
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-12 11:35:36 +0100

    add

--- telnet/app/load_dash.sh
+++ telnet/app/load_dash.sh
@@ -4,10 +4,12 @@ KIBANA_HOST="localhost"
 KIBANA_PORT="5601"
 
 # Indica il file JSON della dashboard
-DASHBOARD_FILE="path/to/your/dashboard.json"
+DASHBOARD_FILE="/dashboards/ftp_dashboard.ndjson"
 
 # Componi l'URL per l'API di Kibana
-KIBANA_API_URL="http://${KIBANA_HOST}:${KIBANA_PORT}/api/saved_objects/_import"
+# KIBANA_API_URL="http://${KIBANA_HOST}:${KIBANA_PORT}/api/saved_objects/_import"
 
 # Invia la richiesta di importazione usando curl
-curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_FILE}
+# curl -X POST "${KIBANA_API_URL}" -H "kbn-xsrf: true" --form file=@${DASHBOARD_FILE}
+
+cat "${DASHBOARD_FILE}"
