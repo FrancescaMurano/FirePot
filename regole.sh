80a21dd8335869a7cb62eb9069957ba550fbc090
FrancescaMurano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-12 08:19:13 -0600

    scripting

--- regole.sh
+++ regole.sh
@@ -1,30 +1,24 @@
 #!/bin/sh
 
-echo 1 > /proc/sys/net/ipv4/ip_forward
+# echo 1 > /proc/sys/net/ipv4/ip_forward
 
-iptables -F
-iptables -t nat -F
-iptables -X
+# iptables -F
+# iptables -t nat -F
+# iptables -X
 
 
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 21 -j ACCEPT
-iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 23 -j ACCEPT
-iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 502 -j ACCEPT
 
-iptables -A DOCKER -d 172.17.0.2
-iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 4564 -j DNAT --to-destination 172.17.0.2:80
-iptables -A DOCKER -d 172.17.0.2
-iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 32237 -j DNAT --to-destination 172.17.0.2:80
 
 # iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 2222 -j ACCEPT
 # iptables -t nat -I PREROUTING -p tcp --dport 23 -j REDIRECT --to-ports 2222
 # iptables -t nat -I OUTPUT -p tcp -o lo --dport 23 -j REDIRECT --to-ports 2222
 
-# iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
-# iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
-# iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/24 -p tcp -m tcp --dport 21 -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/24 -p tcp -m tcp --dport 23 -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/24 -p tcp -m tcp --dport 502 -j ACCEPT
+
 
 # iptables -t nat -I PREROUTING -p tcp --dport 22 -j REDIRECT --to-ports 2222
 # iptables -t nat -I OUTPUT -p tcp -o lo --dport 22 -j REDIRECT --to-ports 2222
--- starter/start.sh
+++ starter/start.sh
@@ -1,4 +1,4 @@
 
-docker-compose up
+# docker-compose up
 
 cd ssh && docker build -t ssh . && docker run --name=ssh -d -p 22:2222 ssh
