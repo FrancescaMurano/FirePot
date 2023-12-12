088825b515d4ec32ca8db8ed8fe7588c68f40ff7
Francesca Murano <mrnfnc99d60b774e@studenti.unical.it>
2023-12-12 15:05:39 +0100

    iptables

--- regole.sh
+++ regole.sh
@@ -13,6 +13,10 @@ iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
 iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 23 -j ACCEPT
 iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 502 -j ACCEPT
 
+iptables -A DOCKER -d 172.17.0.2
+iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 4564 -j DNAT --to-destination 172.17.0.2:80
+iptables -A DOCKER -d 172.17.0.2
+iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport 32237 -j DNAT --to-destination 172.17.0.2:80
 
 # iptables -A INPUT -s 192.168.0.0/24 -p tcp -m tcp --dport 2222 -j ACCEPT
 # iptables -t nat -I PREROUTING -p tcp --dport 23 -j REDIRECT --to-ports 2222
